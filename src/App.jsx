c1bb7fc30333b2a7f2af57262bbddcdafe0780a7
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-01 00:42:32 +0600

    updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from './Components/Card';
 
 function App() {
   return (
-    <div className="grid place-content-center m-auto w-fit">
+    <div className="grid place-content-center mt-5 mb-5 m-auto w-[1000px]">
       <Card />
     </div>
   );
