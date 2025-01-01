e756cc075cb0f73d41bd8aee3a8be0e9c1ae3e4a
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-01 23:19:18 +0600

    added the chat list some style

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,26 @@ function App() {
       <Card />
 
       {/* BMI Charts */}
-      <ol className="text-sm md:text-lg list-decimal p-5 ">
-        <li className="">
+      <ol className="text-sm md:text-lg list-decimal card bg-base-100 shadow-xl py-5 px-10 border rounded-md border-t-1 border-black/10 w-fit ">
+        <li>
           <strong>Underweight</strong>: Less than 18.5
         </li>
-        <li className="">
+        <li>
           <strong>Healthy weight</strong>: 18.5 to 24.9
         </li>
-        <li className="">
+        <li>
           <strong>Overweight</strong>: 25 to 29.9
         </li>
-        <li className="">
+        <li>
           <strong>Obesity</strong>: 30 or greater
         </li>
-        <li className="">
+        <li>
           <strong>Class 1 obesity</strong>: 30 to less than 35
         </li>
-        <li className="">
+        <li>
           <strong>Class 2 obesity</strong>: 35 to less than 40
         </li>
-        <li className="">
+        <li>
           <strong>Class 3 obesity (severe obesity)</strong>: 40 or greater
         </li>
       </ol>
