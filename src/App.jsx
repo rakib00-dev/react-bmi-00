4701d122f179d7766e3a9b47a718e5a9acfdc914
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-01 23:15:22 +0600

    added numbers to the list

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,33 @@ import Card from './Components/Card';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center mt-5 mb-5 m-auto w-11/12">
+    <div className="flex flex-col justify-center items-center mt-5 mb-5 m-auto w-11/12 gap-5">
       <Card />
 
       {/* BMI Charts */}
-      <ul>
-        <li>
+      <ol className="text-sm md:text-lg list-decimal p-5 ">
+        <li className="">
           <strong>Underweight</strong>: Less than 18.5
         </li>
-        <li>
+        <li className="">
           <strong>Healthy weight</strong>: 18.5 to 24.9
         </li>
-        <li>
+        <li className="">
           <strong>Overweight</strong>: 25 to 29.9
         </li>
-        <li>
+        <li className="">
           <strong>Obesity</strong>: 30 or greater
         </li>
-        <li>
+        <li className="">
           <strong>Class 1 obesity</strong>: 30 to less than 35
         </li>
-        <li>
+        <li className="">
           <strong>Class 2 obesity</strong>: 35 to less than 40
         </li>
-        <li>
+        <li className="">
           <strong>Class 3 obesity (severe obesity)</strong>: 40 or greater
         </li>
-      </ul>
+      </ol>
 
       {/* result */}
       <div>
