9ae7d67448c82e96f8ab3d03859d7945dc64fb2a
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-01 17:59:58 +0600

    updated the design and more

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,21 +3,21 @@ const Card = () => {
     <div className="w-full max-w-sm p-4 py-10 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
       <form className="space-y-6" action="#">
         <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-          Sign in to our platform
+          BMI - Body mass index
         </h5>
         <div>
           <label
             for="email"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your email
+            Your Heihgt
           </label>
           <input
             type="email"
             name="email"
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            placeholder="name@company.com"
+            placeholder="64 cm"
             required
           />
         </div>
@@ -26,13 +26,13 @@ const Card = () => {
             for="password"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your password
+            Your Weight
           </label>
           <input
             type="password"
             name="password"
             id="password"
-            placeholder="••••••••"
+            placeholder="44 kg"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
             required
           />
@@ -42,7 +42,7 @@ const Card = () => {
           type="submit"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Login to your account
+          Calculate
         </button>
       </form>
     </div>
